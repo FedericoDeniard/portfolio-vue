52242744746c0a28a98b5fe950461b98dca5d33e
FedericoDeniard <fededeniard@gmail.com>
2024-10-26 09:46:06 -0300

    first commit - Navbar component

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import NavBar from "./components/NavBar.vue";
+
+const handleSectionClick = (section: string) => {
+  console.log(section);
+};
+</script>
+
+<template>
+  <NavBar
+    :title="'Portfolio'"
+    :sections="['Home', 'About', 'Projects', 'Contact']"
+    @onSectionClick="handleSectionClick"
+  />
+</template>
+
+<style scoped></style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  sections: {
+    type: Array as () => string[],
+    default: () => [],
+  },
+});
+
+const emit = defineEmits(["onSectionClick"]);
+const handleClick = (section: string) => {
+  emit("onSectionClick", section);
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">{{ title }}</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li
+            class="nav-item"
+            v-for="(section, index) in sections"
+            :key="index"
+          >
+            <a
+              class="nav-link active"
+              aria-current="page"
+              :href="`#${section}`"
+              @click.prevent="handleClick(section)"
+              >{{ section }}</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
